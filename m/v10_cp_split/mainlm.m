bc005b6d0328327e1e94d2add674ae85956c9e41
bli1 <lby2906@gmaill.com>
2017-04-20 09:53:05 -0400

    [] = gen_Dtable_worstCase(Dtable_schedulable;

---
+++ m/v10_cp_split/mainlm.m
@@ -0,0 +1,45 @@
+function [] = mainlm()
+    fileid = fopen('price.txt','r');
+    formatspec = '%f';
+    price = fscanf(fileid, formatspec);
+    price = price';
+
+
+    %%%battery 
+    %%%(1)=total_max_cap 
+    %%%(2)=init_cap
+    %%%(3)=end_cap
+    %%%(4)=smallest_step
+    %%%(5)=largest_step
+    fileid = fopen('battery.txt','r');
+    formatspec = '%f';
+    battery = fscanf(fileid, formatspec);
+    
+    %% pravicy = lamda value
+    fileid = fopen('privacy.txt','r');
+    formatspec = '%f';
+    lamda = fscanf(fileid, formatspec);
+    
+    [runtime_schedulable, power_schedulable, runtime_nonschedulable, ...
+        power_nonschedulable, price] = prepare_data(price);
+    
+    
+    Bvar_total = zeros(3, 5);
+    Bvar_total(1, :) = [650, 700, 750, 800, 850];
+    Bvar_step = zeros(3, 5);
+    Bvar_step(1, :) = [250, 255, 260, 265, 270];
+    lamda_var = zeros(3, 5);
+    lamda_var(1, :) = [90, 91, 92, 93, 94];
+    
+    for ld = 1:5
+        disp('#############################################################');
+        disp('Lambda');
+        disp(lamda_var(1, ld));
+        lamda = lamda_var(1, ld);
+        [lamda_var(2, ld), lamda_var(3, ld)] = smartHome(price, runtime_schedulable', power_schedulable', ...
+            runtime_nonschedulable', power_nonschedulable', battery, lamda);
+    end
+    save('lamda_var.txt','lamda_var','-ASCII');
+
+end
+
