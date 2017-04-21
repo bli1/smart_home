6986a6f447dd4a5df86803d3c35d287ee010f7ae
Boyang Li <lby2906@gmail.com>
2017-04-21 13:28:39 -0400

    results 3 for Btotal

--- m/v10_cp_split/mainbt.m
+++ m/v10_cp_split/mainbt.m
@@ -24,7 +24,7 @@ function [] = mainbt()
         power_nonschedulable, price] = prepare_data(price);
     
     Bvar_total = zeros(3, 5);
-    Bvar_total(1, :) = [85, 100, 110, 120, 130];
+    Bvar_total(1, :) = [80, 81, 82, 83, 84];
     Bvar_step = zeros(3, 5);
     Bvar_step(1, :) = [30, 35, 40, 45, 50];
     lamda_var = zeros(3, 5);
